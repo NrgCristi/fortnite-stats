4bcfd93291865af06513c97fac7d3a268b5934b1
CristianCosticea <97525096+CristianCosticea@users.noreply.github.com>
2022-06-26 13:09:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+
+
+user = input("What user would you like to search for? ")
+
+r = requests.get("https://fortnite-api.com/v2/stats/br/v2")
+data = r.json()
+with open(f'stats.json', 'w') as f: 
+  json.dump(data, f, indent=3)
+print("Data Has Been Dumped Into File Directory stats.json")
